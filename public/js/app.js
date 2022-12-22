9596fa7bad71c9118d6c74b79c2e743a6289b815
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-22 11:11:53 +0100

    exo 2 incrémenter par deux

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,17 @@
 // EXO1 en js déclare une varibale qui se nomme 'multiplicateur' valant 5 et utilise une boucle for pour créer une table de multiplication allant jusqu'a 9 et apparaissant dans la console sous le format 5 x 1 = 5 etc.
 
+// let multiplicateur = 5;
+
+// for (let index = 0; index <=10; index++) {
+//     console.log(`${multiplicateur} * ${index} = ${multiplicateur * index}`);
+    
+// }
+
+// EXO2 la même chose sauf qu'on incrémente par deux
+
 let multiplicateur = 5;
 
-for (let index = 0; index <=10; index++) {
+for (let index = 0; index <=10; index+=2) {
     console.log(`${multiplicateur} * ${index} = ${multiplicateur * index}`);
     
 }
