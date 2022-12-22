6b5abe1b6d944b0c29a29256fca6136f98a2f398
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-22 12:30:57 +0100

    exo 5 tableau sommes - grossesSommes

--- public/js/app.js
+++ public/js/app.js
@@ -25,16 +25,33 @@
 
 // EXO 4 en js créer un tableau avec les prénoms des gens de la classe ensuite avec l'aide d'une boucle, mettre les prénomns ayant plus de 5 caractères dans un second tableau qui se nomme 'longPrenoms'
 
-let prenoms = ['Quentin','Bachir','Sebastien','Brandon','Charlotte','Denis','Yassine'];
+// let prenoms = ['Quentin','Bachir','Sebastien','Brandon','Charlotte','Denis','Yassine'];
 
-let longPrenoms = [];
+// let longPrenoms = [];
 
-for (let index = 0; index < prenoms.length; index++) {
+// for (let index = 0; index < prenoms.length; index++) {
     
-    if (prenoms[index].length>5) {
-        longPrenoms.push(prenoms[index]);
+//     if (prenoms[index].length>5) {
+//         longPrenoms.push(prenoms[index]);
         
+//     }
+// }
+// console.log(prenoms);
+// console.log(longPrenoms);
+
+// EXO 5 en js créer un tableau qui se nomme 'sommes' avec une multitude de valeurs numérique ensuite créer un tableau qui se nomme 'grossesSommes' et si la valeur excède 60, pousser les dabs ce tableau et les retirer du tableau sommes
+
+let sommes = [14, 7 , 97, 65 , 247 , 67 , 101 , 4 , 34 ,78 , 1 ,900 , 0, 11 , 32 , 23 , 61];
+
+let grossesSommes =[];
+
+for (let index = 0; index < sommes.length; index++) {
+    
+    if (sommes[index]>60) {
+        grossesSommes.push(sommes[index]);
+        sommes.splice(index, 1);
+        index--; 
     }
 }
-console.log(prenoms);
-console.log(longPrenoms);
+console.log(sommes);
+console.log(grossesSommes);
